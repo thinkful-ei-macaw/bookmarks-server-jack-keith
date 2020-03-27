f8f743bcba6a5b2b7dab5bf01df67ca12e6c8ca2
Jack Lansing <jack.lansing@live.com>
2020-03-27 14:30:06 -0400

    added authorization and logging to app.js

--- src/app.js
+++ src/app.js
@@ -3,21 +3,38 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-const bookmarksRouter = require('./bookmarks/bookmarks-router')
+const logger = require('logger');
+const bookmarksRouter = require('./bookmarks/bookmarks-router');
 const { NODE_ENV } = require('./config');
 
 const app = express();
 
 const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 
-
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-app.use('/bookmarks', bookmarksRouter)
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_KEY;
+  const authToken = req.get('Authorization');
 
+  if (authToken && !authToken.toLowerCase().startsWith('bearer ')) {
+    logger.error(`Invalid authorization method on path: ${req.path}`);
+    return res.status(400).json({
+      error: 'Invalid Authorization Method: Must use Bearer strategy'
+    });
+  }
 
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+
+  next();
+});
+
+app.use('/bookmarks', bookmarksRouter);
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
