789701334ae26f4a4e012d1792ee7044c59dd5ad
Jack Lansing <jack.lansing@live.com>
2020-04-02 12:39:02 -0400

    removed comments

--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -25,12 +25,10 @@ bookmarksRouter
       logger.error('Url length is wrong');
       return res.status(400).send('Url length must be 5 or greater');
     }
-    //if they pass a letter like a, Number(rating) will return NaN
     if (!Number(rating)) {
       logger.error('Rating is not a number');
       return res.status(400).send('Rating must be a number');
     }
-    //+ is a Uninary operator converts to number value
     if (+rating < 1 || +rating > 5) {
       logger.error('Rating is not between 1 and 5');
       return res
@@ -51,17 +49,15 @@ bookmarksRouter
     logger.info(`Bookmark with id ${id} created`);
     res
       .status(201)
-      .location(`http://localhost:8000/bookmarks/${id}`) //Comes back as a header in postman
+      .location(`http://localhost:8000/bookmarks/${id}`)
       .json(bookmark);
   });
 
-//Note express finds the first matching route
 bookmarksRouter
   .route('/:id')
   .get((req, res) => {
     const { id } = req.params;
     const bookmark = bookmarksData.find(bookmark => bookmark.id === id);
-    //since bookmarks is not found it is undefined which is a falsy value
     if (!bookmark) {
       logger.error(`Bookmark with id ${id} was not found`);
       return res.status(404).send('404 NOT FOUND');
