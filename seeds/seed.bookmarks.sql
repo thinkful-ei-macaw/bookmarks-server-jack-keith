a0dbe8641450205722a054e460a90db398485ad2
Jack Lansing <jack.lansing@live.com>
2020-04-02 11:54:57 -0400

    added postgrator, initial migration and seed files, knex and pg

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,12 @@
+insert into bookmarks (title, site_url, site_description, rating)
+values
+('Google', 'https://www.google.com', 'The best search engine', 5),
+('Yahoo', 'https://www.yahoo.com', 'I mean its ok', 1),
+('Youtube', 'https://www.youtube.com', 'Great timekiller', 5),
+('Gmail', 'https://www.gmail.com', 'Mail by Google', 5),
+('Reddit', 'https://www.reddit.com', 'The best feedback loop on the net', 4),
+('Thinkful', 'https://www.thinkful.com', 'Already running out of ideas', 4),
+('Ask jeeves', 'http://www.askjeeves.com', 'Not sure this exists still', 2),
+('Github', 'https://www.github.com', 'Simply the best', 5),
+('Quora', 'https://www.quora.com/', 'Dont even think about it', 1),
+('Alligator.io', 'https://www.alligator.io', 'Great learning site', 5);
